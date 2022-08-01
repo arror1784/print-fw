66d62c94cfde795676e4bb25c3b28d8b10419f4e
arror1784 <arror1784@gmail.com>
2022-08-01 14:02:27 +0900

    test code

--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
-const binding = require('bindings')
-const addOn = binding("rgbTrans")
+console.log(require('node-addon-api').gyp)
+console.log(require('node-addon-api').include)
 
-function test(){
-    console.log(addOn.transRgbToBase64("/home/jsh/0.png",0,1,true))
+const binding = require('bindings')
+// const addOn = binding("rgbTrans")
+const addOn = binding("wifiModule")
+
+async function test(){
+    // console.log(addOn.transRgbToBase64("/home/jsh/0.png",0,1,true))
+    addOn.init()
+    addOn.onData((asd)=>{
+        console.log(asd)
+    })
+    addOn.scan()
+
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+    await delay(500000)
 }
 test()
 module.exports = addOn;
