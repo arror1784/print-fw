5eeb0f23fd2edef5af722c94a0f712f2fb209f9d
arror1784 <arror1784@gmail.com>
2022-08-16 18:20:53 +0900

    ipc function add

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,7 +1,7 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron'
 import { DirOrFile } from './ipc/filesystem'
 import { FileSystemCH, ProductCH, ResinCH, WorkerCH,WifiCH } from './ipc/cmdChannels';
-import {WifiInfo} from '../cpp/wifiModule';
+import {WifiCallbackType, WifiInfo} from '../cpp/wifiModule';
 let _id = 0
 
 interface EventListener{
@@ -46,8 +46,9 @@ interface electronApiInterface {
     unLockRM: () => void;
     requestPrintInfoRM: () => void;
     shutdownRM: () => void;
-    connectWifiRM : (ssid:string,bssid:string,passwd:string|null) => void;
+    connectWifiRM : (ssid:string,bssid:string,passwd:string|undefined) => void;
     disconnectWifiRM : () => void;
+    scanWifiRM : () => void;
 
     onWorkingStateChangedMR: (callback:(event:IpcRendererEvent,state: string) => void) => EventListener;
     onPrintInfoMR: (callback:(event:IpcRendererEvent,state: string, material: string, 
@@ -58,6 +59,8 @@ interface electronApiInterface {
     onStartErrorMR: (callback:(event:IpcRendererEvent,error: string) => void) => EventListener;
     onProgressMR: (callback:(event:IpcRendererEvent,progress: number) => void) => EventListener;
     onStatusChangeMR: (callback:(event:IpcRendererEvent,status: WifiInfo)=>void) => EventListener;
+    onWifiListChangeMR: (callback:(evnet:IpcRendererEvent,wifiList: WifiInfo[]) => void) => EventListener;
+    onWifiNoticeMR: (callback:(event:IpcRendererEvent,type:WifiCallbackType,value:number)=>void ) => EventListener;
 
     removeListener : (listener:EventListener) => void;
     removeAllListner : (channel:string) => void;
@@ -77,9 +80,10 @@ const exposedApi: electronApiInterface = {
     printCommandRM: (cmd :string) => ipcRenderer.send(WorkerCH.commandRM,cmd),
     unLockRM: () => ipcRenderer.send(WorkerCH.unlockRM),
     requestPrintInfoRM: () => ipcRenderer.send(WorkerCH.requestPrintInfoRM),
-    shutdownRM: () => ipcRenderer.send(ProductCH.onShutDownRM),
-    connectWifiRM: (ssid:string,bssid:string,passwd:string|null) => ipcRenderer.send(WifiCH.connectWifiRM,ssid,bssid,passwd),
+    shutdownRM: () => ipcRenderer.send(ProductCH.shutDownRM),
+    connectWifiRM: (ssid:string,bssid:string,passwd:string|undefined) => ipcRenderer.send(WifiCH.connectWifiRM,ssid,bssid,passwd),
     disconnectWifiRM: () => ipcRenderer.send(WifiCH.disconnectWifiRM),
+    scanWifiRM: () => ipcRenderer.send(WifiCH.scanWifiRM),
 
     onWorkingStateChangedMR: (callback:(event: IpcRendererEvent,state: string) => void) => {return eventADD(WorkerCH.onWorkingStateChangedMR,callback)},
     onPrintInfoMR: (callback:(event:IpcRendererEvent,state: string, material: string, 
@@ -90,6 +94,8 @@ const exposedApi: electronApiInterface = {
     onStartErrorMR: (callback:(event:IpcRendererEvent,error: string) => void) => {return eventADD(WorkerCH.onStartErrorMR,callback)},
     onProgressMR: (callback:(event:IpcRendererEvent,progress: number) => void) => {return eventADD(WorkerCH.onProgressMR,callback)},
     onStatusChangeMR: (callback:(event:IpcRendererEvent,status: WifiInfo) => void) => {return eventADD(WifiCH.onStatusChangeMR,callback)},
+    onWifiListChangeMR: (callback:(event:IpcRendererEvent,wifiList:WifiInfo[]) => void) => {return eventADD(WifiCH.onWifiListChangeMR,callback)},
+    onWifiNoticeMR: (callback:(event:IpcRendererEvent,type:WifiCallbackType,value:number)=>void) => {return eventADD(WifiCH.onWifiNoticeMR,callback)},
     
     removeListener : (listener:EventListener) => eventRemove(listener),
     removeAllListner : (channel:string) => ipcRenderer.removeAllListeners(channel),
