dff4612f6c5757f7dfeb6beb2a0e1c95cca849d3
arror1784 <arror1784@gmail.com>
2022-08-17 17:56:16 +0900

    useRef add

--- src/pages/Wifi.tsx
+++ src/pages/Wifi.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from '../components/Button';
 
 import Footer from '../layout/Footer';
