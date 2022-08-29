de5b6a631e6ac964d5cd8f4ea0eade25e2a23a56
arror1784 <arror1784@gmail.com>
2022-08-29 13:53:54 +0900

    fix url and change return value

--- electron/resinControl.ts
+++ electron/resinControl.ts
@@ -2,7 +2,6 @@ import { app, net } from "electron";
 import { getPrinterSetting } from "./json/printerSetting";
 import { ResinSetting } from "./json/resin";
 
-import request from 'request'
 import requestPromise from 'request-promise'
 import { getProductSetting } from "./json/productSetting";
 
@@ -12,7 +11,7 @@ class ResinControl{
     constructor(){
     }
 
-    private url :string = "https://services.hix.co.kr/resin/update/"
+    private url :string = "https://services.hix.co.kr/resin/"
     public getResinList():string[]{
         return getPrinterSetting().data.resinList
     }
@@ -20,6 +19,7 @@ class ResinControl{
     public getResinLastupdate():string{
         let lastUpdate = new Date(0)
         for (const i of getPrinterSetting().data.resinList) {
+            
             let tmpid =  new ResinSetting(i)
             if(!tmpid.last_update)
                 continue
@@ -31,38 +31,51 @@ class ResinControl{
         return lastUpdate.toUTCString()
     }
 
-    public async checkAvailableToUpdateNetwork():Promise<boolean>{
+    public async checkAvailableToUpdateNetwork():Promise<string|null>{
 
         // const request = net.request({method: 'GET',protocol: 'https:',hostname: 'services.hix.co.kr',  path: '/resin/update/C10'})
-        let rt = false
         let currentLastUpdate = new Date(this.getResinLastupdate())
-        
-        await request({url:this.url+getProductSetting().data.product.toUpperCase(),json:true},(error,response)=>{
+        let serverLastUpdate : string|null = null
+
+        await requestPromise({url:this.url+"update/"+getProductSetting().data.product.toUpperCase(),json:true},(error,response)=>{
             if(error){
-                rt = false
                 return
             }
+            if(getPrinterSetting().data.resinList.length > Object.keys(response.body).length){
+                currentLastUpdate = new Date(0)
+                for (const i of response.body) {
+                    for (const name of Object.keys(i)) {
+                        let temptime = new Date(i[name])
+
+                        if(currentLastUpdate.getTime() < temptime.getTime()){
+                            currentLastUpdate = temptime
+                            serverLastUpdate = temptime.toLocaleString()
+                        }
+                    }
+                }
+                return
+            } // if resin removed
             for (const i of response.body) {
                 for (const name of Object.keys(i)) {
                     let temptime = new Date(i[name])
-
+                    console.log(currentLastUpdate.getTime(),temptime.getTime())
                     if(currentLastUpdate.getTime() < temptime.getTime()){
-                        rt = true
-                        return
+                        currentLastUpdate = temptime
+                        serverLastUpdate = temptime.toLocaleString()
                     }
                 }
-            }
+            } // if resin update or add
 
         })
         
-        return rt
+        return serverLastUpdate
     }
 
     public async resinUpdate(){
 
         let rt = false
 
-        await requestPromise({url:"https://services.hix.co.kr/resin/download/C10",json:true},(error,response)=>{
+        await requestPromise({url:this.url+"download/"+getProductSetting().data.product.toUpperCase(),json:true},(error,response)=>{
             if(error){
                 rt = false
                 return
@@ -76,6 +89,7 @@ class ResinControl{
             getPrinterSetting().saveFile()
 
             for (const name of Object.keys(response.body)) {
+                console.log(name)
                 let a = new ResinSetting(name,JSON.stringify(response.body[name]))
                 a.saveFile()
             }
